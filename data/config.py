495811377425eb9b3ec633bd7ceef0777dbce4a3
SM P <zzererer0471@gmail.com>
2019-07-16 15:15:29 +0900

    190716 commit-1 data/config.py
    - HOME의 위치를 "./"로 수정

--- data/config.py
+++ data/config.py
@@ -2,8 +2,8 @@
 import os.path
 
 # gets home dir cross platform
-HOME = os.path.expanduser("~")
-
+# HOME = os.path.expanduser("~")
+HOME = './'
 # for making bounding boxes pretty
 COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
           (0, 255, 255, 128), (255, 0, 255, 128), (255, 255, 0, 128))
